f0c589a48e5519e88e54fefe6f096db86b49b0a3
debater-coder <52619668+debater-coder@users.noreply.github.com>
2022-07-09 22:02:34 +1000

    init

--- main.js
+++ main.js
@@ -1,8 +1,7 @@
 import "./style.css";
 import Experience from "./Experience";
 import * as THREE from "three";
-import waterVertexShader from "./shaders/water/vertex.glsl?raw";
-import waterFragmentShader from "./shaders/water/fragment.glsl?raw";
+import { TwistyPlayer } from "cubing/twisty";
 
 class App extends Experience {
   constructor() {
@@ -14,61 +13,22 @@ class App extends Experience {
 
     this.camera.position.set(6, 4, 8);
 
-    this.sun = new THREE.DirectionalLight("#ffffff", 1);
-    this.sun.castShadow = true;
-    this.sun.shadow.camera.far = 15;
-    this.sun.shadow.mapSize.set(1024, 1024);
-    this.sun.shadow.normalBias = 0.05;
-    this.sun.position.set(3.5, 2, -1.25);
-    this.sun.position.set(3.5, 2, -1.25);
-    this.scene.add(this.sun);
-
-    this.sea = new THREE.Mesh(
-      new THREE.PlaneGeometry(50, 50, 128, 128),
-      new THREE.ShaderMaterial({
-        vertexShader: waterVertexShader,
-        fragmentShader: waterFragmentShader,
-      })
+    const player = document.body.appendChild(
+      new TwistyPlayer({
+        alg: "R U R' U' R' F R2 U' R' U' R U R' F'",
+        puzzle: "3x3x3",
+        hintFacelets: "none",
+      }),
     );
-    this.sea.rotation.x = -Math.PI / 2;
-    this.scene.add(this.sea);
 
-    this.resources
-      .startLoading([
-        {
-          name: "boat",
-          type: "gltfModel",
-          path: "/boat_josefa/scene.gltf",
-        },
-        {
-          name: "environmentMap",
-          type: "cubeTexture",
-          path: [
-            "/environment_map/px.png",
-            "/environment_map/nx.png",
-            "/environment_map/py.png",
-            "/environment_map/ny.png",
-            "/environment_map/pz.png",
-            "/environment_map/nz.png",
-          ],
-        },
-      ])
-      .then(() => {
-        this.scene.background = this.resources.items["environmentMap"];
-        this.boat = this.resources.items["boat"].scene;
-        this.boat.position.y = -0.5;
-        this.scene.add(this.boat);
+    player.style.height = "3.5em"
 
-        this.applyEnvironmentMap(this.resources.items["environmentMap"]);
-      });
-  }
+    player.experimentalCurrentThreeJSPuzzleObject(() => {
 
-  update() {
-    super.update();
-    if (this.boat) {
-      this.boat.rotation.x = Math.sin(this.time.elapsed * 0.0005) * 0.1;
-      this.boat.rotation.y = Math.cos(this.time.elapsed * 0.0005) * 0.1;
-    }
+    }).then((puzzle3d) => {
+      this.scene.add(puzzle3d)
+    })
+
   }
 }
 
